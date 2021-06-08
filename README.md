b318d91bdf400b581c62b904c69a0f6d31d18170
Ralf S. Engelschall <rse@engelschall.com>
2021-06-08 09:09:45 +0200

    start a new utility for managing the configurations of OBS Studio

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# obs-config
-OBS Studio Configuration Manager
+
+obs-config
+==========
+
+**OBS Studio Configuration Manager**
+
+<p/>
+<img src="https://nodei.co/npm/obs-config.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/obs-config.png" alt=""/>
+
+Abstract
+--------
+
+This is a small utility for managing the configuration files and
+referenced media assets of [**OBS Studio**](https://obsproject.com).
+
+Installation
+------------
+
+- download pre-built binary for Windows (x64):<br/>
+  https://github.com/rse/obs-config/releases/download/0.9.0/obs-config-win-x64.exe
+
+- download pre-built binary for macOS (x64):<br/>
+  https://github.com/rse/obs-config/releases/download/0.9.0/obs-config-mac-x64
+
+- download pre-built binary for GNU/Linux (x64):<br/>
+  https://github.com/rse/obs-config/releases/download/0.9.0/obs-config-lnx-x64
+
+- via Node.js/NPM for any platform:<br/>
+  `$ npm install -g obs-config`
+
+Usage
+-----
+
+```
+$ obs-config \
+  [-v|--verbose <level>]
+  [-v|--verbose <level>]
+  [-d|--directory <config-directory>]
+  [-p|--profile <profile-pattern>[,...]]
+  [-c|--collection <collection-pattern>[,...]]
+  <command> [...]
+$ obs-config [...] locate             # locate and display all external assets
+$ obs-config [...] consolidate        # locate and internalize external assets
+$ obs-config [...] relocate           # relocate all internal assets (after directory change)
+$ obs-config [...] export <zip-file>  # export configuration and internal assets
+$ obs-config [...] import <zip-file>  # import configuration and internal assets
+```
+
+License
+-------
+
+Copyright &copy; 2021 Dr. Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ obs-config.js
@@ -0,0 +1,153 @@
+#!/usr/bin/env node
+/*!
+**  obs-config -- OBS Studio Configuration Manager
+**  Copyright (c) 2021 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*  external requirements  */
+const yargs       = require("yargs")
+const chalk       = require("chalk")
+const stripAnsi   = require("strip-ansi")
+// const jsYAML      = require("js-yaml")
+
+/*  own package information  */
+// const my          = require("./package.json")
+
+;(async () => {
+    /*  parse command-line options  */
+    const usage =
+        "Usage: obs-config " +
+        " [-v|--verbose <level>]" +
+        " [-d|--directory <config-directory>]" +
+        " [-p|--profile <profile-pattern>[,...]]" +
+        " [-c|--collection <collection-pattern>[,...]]" +
+        " <command> [...]\n" +
+        "Usage: obs-config [...] locate             # locate and display all external assets\n" +
+        "Usage: obs-config [...] consolidate        # locate and internalize external assets\n" +
+        "Usage: obs-config [...] relocate           # relocate all internal assets (after directory change)\n" +
+        "Usage: obs-config [...] export <zip-file>  # export configuration and internal assets\n" +
+        "Usage: obs-config [...] import <zip-file>  # import configuration and internal assets"
+    const opts = yargs()
+        .parserConfiguration({
+            "set-placeholder-key": true,
+            "halt-at-non-option":  true
+        })
+        .usage(usage)
+        .option("v", {
+            alias:    "verbose",
+            type:     "number",
+            describe: "level of verbose output",
+            nargs:    1,
+            default:  0
+        })
+        .option("d", {
+            alias:    "directory",
+            type:     "string",
+            describe: "OBS Studio configuration directory",
+            default:  ""
+        })
+        .option("p", {
+            alias:    "profile",
+            type:     "string",
+            describe: "OBS Studio profile name pattern list",
+            default:  "*"
+        })
+        .option("c", {
+            alias:    "collection",
+            type:     "string",
+            describe: "OBS Studio scene collection name pattern list",
+            default:  "*"
+        })
+        .version(false)
+        .help(true)
+        .showHelpOnFail(true)
+        .strict(true)
+        .parse(process.argv.slice(2))
+    if (opts._.length < 1) {
+        process.stderr.write(`${usage}\n`)
+        process.exit(1)
+    }
+
+    /*  helper function for verbose log output  */
+    const logLevels = [ "NONE", chalk.blue("INFO"), chalk.yellow("DEBUG") ]
+    const log = (level, msg) => {
+        if (level > 0 && level < logLevels.length && level <= opts.verbose) {
+            msg = `pptx-surgeon: ${chalk.blue(logLevels[level])}: ${msg}\n`
+            if (opts.outputNocolor || !process.stderr.isTTY)
+                msg = stripAnsi(msg)
+            process.stderr.write(msg)
+        }
+    }
+
+    /*  dispatch commands  */
+    const command = opts._[0]
+    if (command === "locate") {
+        if (opts._.length !== 1) {
+            process.stderr.write("ERROR: invalid number of arguments\n")
+            process.stderr.write(`${usage}\n`)
+            process.exit(1)
+        }
+        log(0, "locate")
+    }
+    else if (command === "consolidate") {
+        if (opts._.length !== 1) {
+            process.stderr.write("ERROR: invalid number of arguments\n")
+            process.stderr.write(`${usage}\n`)
+            process.exit(1)
+        }
+        log(0, "consolidate")
+    }
+    else if (command === "relocate") {
+        if (opts._.length !== 1) {
+            process.stderr.write("ERROR: invalid number of arguments\n")
+            process.stderr.write(`${usage}\n`)
+            process.exit(1)
+        }
+        log(0, "relocate")
+    }
+    else if (command === "export") {
+        if (opts._.length !== 2) {
+            process.stderr.write("ERROR: invalid number of arguments\n")
+            process.stderr.write(`${usage}\n`)
+            process.exit(1)
+        }
+        const zipfile = opts._[1]
+        log(0, `export: ${zipfile}`)
+    }
+    else if (command === "import") {
+        if (opts._.length !== 2) {
+            process.stderr.write("ERROR: invalid number of arguments\n")
+            process.stderr.write(`${usage}\n`)
+            process.exit(1)
+        }
+        const zipfile = opts._[1]
+        log(0, `import: ${zipfile}`)
+    }
+
+    /*  gracefully terminate  */
+    process.exit(0)
+})().catch((err) => {
+    /*  fatal error  */
+    process.stderr.write(`pptx-surgeon: ${chalk.red("ERROR:")} ${err.stack}\n`)
+    process.exit(1)
+})
+
